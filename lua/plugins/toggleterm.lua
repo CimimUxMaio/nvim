5119d87faf116f341c88e5211efe2a419addfa64
CimimUxMaio <cimimuxmaio@gmail.com>
2022-03-21 15:39:41 -0400

    disable terminal toggle in insert mode

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -15,7 +15,7 @@ toggleterm.setup({
     shade_terminals = true,
     shading_factor = 2,
     start_in_insert = true,
-    insert_mappings = true,
+    insert_mappings = false,  -- dont toggle in insert mode
     persist_size = true,
     direction = "horizontal",
     close_on_exit = true,
