846d1798ed9af720da65aab8f97c57c662e21e16
CimimUxMaio <cimimuxmaio@gmail.com>
2022-03-22 03:48:02 -0300

    Secondary terminal + improved terminal toggles

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -8,51 +8,96 @@ end
 
 
 toggleterm.setup({
-    size = 10,
-    open_mapping = "|",
+    size = 15,
+    -- open_mapping = "|",
     hide_numbers = true,
     shade_filetypes = {},
     shade_terminals = true,
     shading_factor = 2,
     start_in_insert = true,
-    insert_mappings = false,  -- dont toggle in insert mode
+    insert_mappings = false,
     persist_size = true,
-    direction = "horizontal",
+    -- direction = "horizontal",
     close_on_exit = true,
-    shell = vim.o.shell
+    shell = vim.o.shell,
+    float_opts = {
+        border = "curved",
+        winblend = 0,
+        highlights = {
+            border = "Normal",
+            background = "Normal"
+        }
+    }
+
 })
 
 
+local keymap_opts = { noremap = true, silent = true }
 
-function _G.set_terminal_keymaps()
-    local opts = { noremap = true }
-    --vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
-    vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
-    vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
-    vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
-    vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
+
+function _G.set_terminal_movement_keymaps(bufnr)
+    --vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], keymap_opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "t", "<C-h>", [[<C-\><C-n><C-W>h]], keymap_opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "t", "<C-j>", [[<C-\><C-n><C-W>j]], keymap_opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "t", "<C-k>", [[<C-\><C-n><C-W>k]], keymap_opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "t", "<C-l>", [[<C-\><C-n><C-W>l]], keymap_opts)
 end
 
-vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+
+function _G.set_terminal_close_keymap(bufnr)
+    vim.api.nvim_buf_set_keymap(bufnr, "t", "<M-t>", "<cmd>close<CR>", keymap_opts)
+end
+
+
 
 local Terminal = require("toggleterm.terminal").Terminal
 
-
-local float_opts = {
-    border = "curved",
-    winblend = 0,
-    highlights = {
-        border = "Normal",
-        background = "Normal"
-    }
+local main_term = Terminal:new {
+    hidden = true,
+    direction = "horizontal",
+    on_open = function(term)
+        set_terminal_movement_keymaps(term.bufnr)
+        set_terminal_close_keymap(term.bufnr)
+    end
 }
 
-local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction="float", float_opts=float_opts })
-function _G.git()
+function _G.main_term_toggle()
+    main_term:toggle()
+end
+
+vim.cmd("command -nargs=0 ToggleMainTerm lua main_term_toggle()")
+utils.nnoremap("|", ":ToggleMainTerm<CR>", { silent = true })
+
+
+
+local secondary_term = Terminal:new {
+    hidden = true,
+    direction = "float",
+    on_open = function(term)
+        set_terminal_close_keymap(term.bufnr)
+    end
+}
+
+function _G.secondary_term_toggle()
+    secondary_term:toggle()
+end
+
+vim.cmd("command -nargs=0 ToggleSecondaryTerm lua secondary_term_toggle()")
+utils.nnoremap("<M-t>", ":ToggleSecondaryTerm<CR>", { silent = true })
+
+
+
+local lazygit = Terminal:new {
+    cmd = "lazygit",
+    hidden = true,
+    direction= "float",
+}
+
+function _G.lazygit_toggle()
     lazygit:toggle()
 end
 
-vim.cmd("command -nargs=0 LazyGit lua git()")
+vim.cmd("command -nargs=0 LazyGit lua lazygit_toggle()")
 
 
-
+-- vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
