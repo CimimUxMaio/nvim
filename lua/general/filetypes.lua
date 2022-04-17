351c583f3fe630f4dbd0e59da9cf1de07e64e007
CimimUxMaio <cimimuxmaio@gmail.com>
2022-04-17 01:44:07 -0300

    custom tab width for specific file types

---
+++ lua/general/filetypes.lua
@@ -0,0 +1,5 @@
+vim.cmd [[
+    autocmd FileType html setlocal shiftwidth=2 tabstop=2
+    autocmd FileType javascript setlocal shiftwidth=2 tabstop=2
+    autocmd FileType typescript setlocal shiftwidth=2 tabstop=2
+]]
--- lua/general/init.lua
+++ lua/general/init.lua
@@ -1,3 +1,4 @@
 
 require('general.settings')
 require('general.mappings')
+require("general.filetypes")
