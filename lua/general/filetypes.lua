69347650839862d5b071efb583c93c798a675583
CimimUxMaio <cimimuxmaio@gmail.com>
2022-06-13 11:00:14 -0300

    Adding JSON files to the list of tab=2 spaces files

--- lua/general/filetypes.lua
+++ lua/general/filetypes.lua
@@ -1,5 +1,7 @@
+
 vim.cmd [[
     autocmd FileType html setlocal shiftwidth=2 tabstop=2
     autocmd FileType javascript setlocal shiftwidth=2 tabstop=2
     autocmd FileType typescript setlocal shiftwidth=2 tabstop=2
+    autocmd FileType json setlocal shiftwidth=2 tabstop=2
 ]]
